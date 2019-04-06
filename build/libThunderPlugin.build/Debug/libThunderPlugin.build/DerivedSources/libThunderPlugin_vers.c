edb344a65a994054c4832914f8b91f0837c21462
Nick Lim <nickycitcit@gmail.com>
2019-04-06 08:03:06 +0800

    test

---
+++ build/libThunderPlugin.build/Debug/libThunderPlugin.build/DerivedSources/libThunderPlugin_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char libThunderPluginVersionString[];
+ extern const double libThunderPluginVersionNumber;
+
+ const unsigned char libThunderPluginVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:libThunderPlugin  PROJECT:libThunderPlugin-1" "\n";
+ const double libThunderPluginVersionNumber __attribute__ ((used)) = (double)1.;
